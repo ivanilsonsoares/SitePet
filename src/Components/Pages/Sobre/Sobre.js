61c31edc44cc4aefd0adb9679f7e4923900bea91
ivanilsonsoares <ivanilson.soares.is@gmail.com>
2021-03-14 11:27:14 -0300

    Page Sobre

--- src/Components/Pages/Sobre/Sobre.js
+++ src/Components/Pages/Sobre/Sobre.js
@@ -1,9 +1,25 @@
 import { Browser } from "../../Common/Browser/Browser";
-
+import { Footer } from "../../Common/Footer/Footer";
+import './Sobre.css'
 export function Sobre(){
     return(
         <div>
             <Browser></Browser>
+            <div className="Banner-Sobre">
+                <div className="info">
+                    <h1>Sobre</h1>
+                    <p>Saiba um pouco mais sobre nós</p>
+                </div>
+            </div>
+            <h1>sdsds</h1>
+            <h1>sdsds</h1>
+            <h1>sdsds</h1>
+            <h1>sdsds</h1>
+            <h1>sdsds</h1>
+            <h1>sdsds</h1>
+            <h1>sdsds</h1>
+
+            <Footer></Footer>
         </div>
     );
 }
